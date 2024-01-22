8fda24d7eac5d16e7b6dbac2ba806806872cf727
SaillisA <dumaine.lisa04@gmail.com>
2024-01-22 15:03:11 +0100

    trigger ok mais soucis gravité

--- Assets/Script/JoueurScript.cs
+++ Assets/Script/JoueurScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class JoueurScript : MonoBehaviour
@@ -9,33 +10,56 @@ public class JoueurScript : MonoBehaviour
     public float vitesse;
 
 
+    public float tempsAttente;
+    public float tempsAttenteNecessaire;
+
+    //Collider
+    public GameObject SolBase;
+
     void Start()
     {
-
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        tempsAttente += Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && tempsAttente > tempsAttenteNecessaire)
         {
             transform.position += Vector3.left * vitesse;
+            tempsAttente = 0;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tempsAttente > tempsAttenteNecessaire)
         {
             transform.position += Vector3.right * vitesse;
+            tempsAttente = 0;
 
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && tempsAttente > tempsAttenteNecessaire)
         {
             transform.position += Vector3.up * vitesse;
-        }
+            tempsAttente = 0;
 
-        /*
-        if (Input.GetKey(KeyCode.DownArrow))
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && tempsAttente > tempsAttenteNecessaire)
         {
             transform.position += Vector3.down * vitesse;
+            tempsAttente = 0;
+
         }
-        */
+
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Oui, tu rentres en collision");
+        if(collision.gameObject.tag == "Sol")
+        {
+            Debug.Log("Je touche le sol !");
+        }
+    }
+
 }
